a733276f5d7023378b35fa34314233724975f05b
Daniel Silva de Souza <danielss2990@gmail.com>
2021-01-26 14:45:45 -0300

    Create auth module

--- frontend-web/src/pages/Auth/components/Login/index.tsx
+++ frontend-web/src/pages/Auth/components/Login/index.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import Buttonicon from '../../../../core/components/ButtonIcon';
 import AuthCard from '../Card';
 import './styles.scss';
 
+type FormData = {
+    username: string;
+    password: string;
+}
+
 
 const Login = () => {
+    const { register, handleSubmit } = useForm<FormData>(); // initialize the hook
+    
+    const onSubmit = (data: FormData) => {
+        console.log(data);
+        //chamar api de autenticação
+    }
+    
     return (
         <AuthCard title="login">
-            <form className="login-form">
+            <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
                 <input
                     type="email"
                     className="form-control input-base margin-bottom-30"
                     placeholder="Email"
+                    name="email" 
+                    ref={register}
                 >
 
                 </input>
@@ -19,7 +34,9 @@ const Login = () => {
                 <input
                     type="password"
                     className="form-control input-base margin-bottom-30"
-                    placeholder="Password"
+                    placeholder="Senha"
+                    name="password" 
+                    ref={register}
                 >
 
                 </input>
